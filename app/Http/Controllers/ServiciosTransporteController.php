f338fc3836b5172446cd4879217700688d2ee2cd
Luiggi Giussepi Moretti Dioses <programador02@ASJ.local>
2023-10-10 15:57:47 -0500

    terminado transportes con API

--- app/Http/Controllers/ServiciosTransporteController.php
+++ app/Http/Controllers/ServiciosTransporteController.php
@@ -12,30 +12,34 @@ class ServiciosTransporteController extends Controller
 {
     public function getServiciosTransporte(Request $request)
     {
-        return $request['idServicioTransporte'];
+        // return $request['idServicioTransporte'];
         $idServicioTransporte = $request['idServicioTransporte'];
-        $existsServicioTransporte = DB::select("select * from trx_ServiciosTransporte where Id= '" . $idServicioTransporte . "';", []);
-        try {
-            
-            $jsonPasajeros = json_encode(['pasajeros' => $request['pasajeros']]);
-            $queryUnidad = "INSERT INTO trx_ServiciosTransporte VALUES(" . $request['unidad'] . ")";
-            DB::unprepared($queryUnidad);
-            $result = DB::select("EXEC sp_Dgm_ServiciosTransporte_TransferirRegistroTransporte ?;", [$jsonPasajeros]);
-            return ['response' => $existsServicioTransporte];
-        } catch (\Throwable $th) {
-            //throw $th;
+        $existsServicioTransporte = DB::select("select count(*) response from trx_ServiciosTransporte where Id= '" . $idServicioTransporte . "';")[0];
+        if ($existsServicioTransporte->response == 1) {
             $newId = "EXECUTE sp_Dgm_Gen_obtenerNuevoId ?,?,?";
             $params = [
                 "trx_ServiciosTransporte",
                 $request['idEmpresa'],
                 $request['idDispositivo']
             ];
-            $nuevoId = DB::select($newId, $params);
-            return ['response' => $existsServicioTransporte];
+            $nuevoId = DB::select($newId, $params)[0];
+            return ['code' => 500, 'newId' => $nuevoId->Detalle, 'response' => strval("AGREGADO CORRECTAMENTE CON ID DIFERENTE")];
+        } else {
+            $jsonPasajeros = json_encode(['pasajeros' => $request['pasajeros']]);
+            $queryUnidad = "INSERT INTO trx_ServiciosTransporte VALUES(" . $request['unidad'] . ")";
+            DB::unprepared($queryUnidad);
+            $result = DB::statement("EXEC sp_Dgm_ServiciosTransporte_TransferirRegistroTransporte ?;", [$jsonPasajeros]);
+            return ['code' => 200, 'newId' => "nada mano", 'response' => strval("AGREGADO CORRECTAMENTE")];
+        }
+        try {
+        } catch (\Throwable $th) {
+            //throw $th;
+            return ['code' => 500, 'response' => strval($th)];
         }
     }
 
-    public function getLogs(){
+    public function getLogs()
+    {
         return DB::select("SELECT * FROM Logs WHERE Parametros LIKE '%000000000EAX%' ORDER BY 1;");
     }
 }
