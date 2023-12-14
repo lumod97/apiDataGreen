45c001568226d3e74878914b74f10d5adbb02e3a
Luiggi Giussepi Moretti Dioses <programador02@ASJ.local>
2023-12-14 11:10:32 -0500

    log creado

--- app/Http/Controllers/TareosController.php
+++ app/Http/Controllers/TareosController.php
@@ -10,13 +10,22 @@ class TareosController extends Controller
 {
     public function insertarTareos(Request $request)
     {
-
         $params = [
             json_encode(['tareos' => $request['tareos']])
         ];
+
+        $logParams = [
+            $request["mac"],
+            $request["user_login"],
+            $request["app"],
+            $request["descripcion"],
+            $request["parametros"]
+        ];
+
         try {
             $data = DB::select("SET NOCOUNT ON; EXEC DataGreenMovil..sp_Dgm_Tareos_TransferirTareo_V2 ?;", $params);
             // return response()->json();
+            DB::statement("insert into Datagreen..Logs values(GETDATE(), ?, ?, ?, ?, ?)",$logParams);
             return ['code' => 200, 'response' => $data];
         } catch (\Throwable $th) {
             //throw $th;
