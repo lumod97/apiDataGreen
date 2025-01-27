a54070e63a3363b141e2fbc61c0e15df190dae24
Luiggi Moretti Dioses <luiggigmd_97@outlook.com>
2025-01-27 01:39:03 -0500

    obtensión de version de minigreen mejorado

--- app/Http/Controllers/MiniGreenController.php
+++ app/Http/Controllers/MiniGreenController.php
@@ -7,19 +7,34 @@ use Illuminate\Support\Facades\DB;
 
 class MiniGreenController extends Controller
 {
-    public function validarVersion(){
-        $data = DB::select("select ISNULL(MAX(id), '0.0.0') last_id FROM DataGreenMovil..LogVersionesMiniGreen");
+    public function validarVersion()
+    {
+        $data = DB::select("WITH SplitVersions AS (
+    SELECT 
+        id,
+        CAST(PARSENAME(id, 4) AS INT) AS major,
+        CAST(PARSENAME(id, 3) AS INT) AS minor,
+        CAST(PARSENAME(id, 2) AS INT) AS patch,
+        CAST(PARSENAME(id, 1) AS INT) AS build
+    FROM DataGreenMovil..LogVersionesMiniGreen
+)
+SELECT TOP 1 id AS last_id
+FROM SplitVersions
+ORDER BY major DESC, minor DESC, patch DESC, build DESC;
+");
         return $data[0]->last_id;
     }
 
     // THIS FUNCTION IS DEPRECATED
-    public function devolverRutaActualizacion(){
+    public function devolverRutaActualizacion()
+    {
         $rutaArchivo = "http://192.168.30.99:8090/DataGreenMovil/MiniGreen1.7.0.apk";
         $nombreArchivo = 'nombre_archivo.apk';
         return response()->download($rutaArchivo, $nombreArchivo);
     }
 
-    public function registrarActualización(Request $request){
+    public function registrarActualización(Request $request)
+    {
         $params = [
             $request['version'],
             $request['usuario'],
