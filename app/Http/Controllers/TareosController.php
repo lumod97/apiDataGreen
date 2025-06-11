c0933db83d798c75839acc6556205b2472d8a542
Luiggi Moretti Dioses 94 <luiggigmd_97@outlook.com>
2025-06-11 11:57:47 -0500

    PRUEBAS CAMBIOS EN EXTORNAR 7

--- app/Http/Controllers/TareosController.php
+++ app/Http/Controllers/TareosController.php
@@ -148,11 +148,11 @@ class TareosController extends Controller
 
             // RETORNAMOS EL RESPONSE
             if ($response->code == '500') {
-                File::append(storage_path('logs/log_500.txt'), PHP_EOL . 'Momento: ' . $currentDate . ' ----- DATA: EJEMPLO ' . PHP_EOL);
+                File::append(storage_path('logs/log_500.txt'), PHP_EOL . 'Momento: ' . $currentDate . ' ----- DATA: ' . stripslashes(json_encode($data)) . PHP_EOL);
                 return ['code' => $response->code, 'response' => $data];
                 // throw new Exception($response);
             } else if ($response->code == '200') {
-                File::append(storage_path('logs/log_success.txt'), PHP_EOL . 'Momento: ' . $currentDate . ' ----- DATA: ' . strval(json_encode($data)) . PHP_EOL);
+                File::append(storage_path('logs/log_success.txt'), PHP_EOL . 'Momento: ' . $currentDate . ' ----- DATA: ' . stripslashes(json_encode($data)) . PHP_EOL);
                 return ['code' => $response->code, 'response' => $data];
             }
         } catch (\Throwable $th) {
