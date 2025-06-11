cd7f139258c26c40b0524eaa390af91e68cecff3
Luiggi Moretti Dioses 94 <luiggigmd_97@outlook.com>
2025-06-11 11:54:57 -0500

    AGREGAR LOGS PARA DETECTAR RESPUESTAS 5

--- app/Http/Controllers/TareosController.php
+++ app/Http/Controllers/TareosController.php
@@ -149,7 +149,7 @@ class TareosController extends Controller
             // RETORNAMOS EL RESPONSE
             if ($response->code == '500') {
                 return ['code' => $response->code, 'response' => $data];
-                File::append(storage_path('logs/log_500.txt'), PHP_EOL . 'Momento: ' . $currentDate . ' ----- DATA: ' . strval(json_encode($data)) . PHP_EOL);
+                File::append(storage_path('logs/log_500.txt'), PHP_EOL . 'Momento: ' . $currentDate . ' ----- DATA: EJEMPLO ' . PHP_EOL);
                 // throw new Exception($response);
             } else if ($response->code == '200') {
                 File::append(storage_path('logs/log_success.txt'), PHP_EOL . 'Momento: ' . $currentDate . ' ----- DATA: ' . strval(json_encode($data)) . PHP_EOL);
